4766aa53afc8d8e3d9c2d5a0be860721f3ecb2cc
Enrique Pinedo <epinedo31@outlook.com>
2023-08-26 20:51:30 -0500

    apiroutes

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,2 +1,23 @@
 // 
 const router = require('express').Router();
+const uuid = require('../helpers/uuid');
+const fs = require("fs");
+
+// Defines GET requests to this route
+router.get('?', async(req, res) => {
+    const dbJson = await JSON.parse(fs.readFileSync("db/db.json"))
+    res.json(dbJson);
+});
+
+// Defines POST requests to this route
+router.post('?', (req, res) => {
+    const dbJson = JSON.parse(fs.readFileSync("db/db.json"))
+    const newFeedback = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+    };
+    dbJson.push(newFeedback);
+    fs.writeFileSync("db/db.json", JSON.stringify(dbJson));
+    res.json(dbJson);
+})
